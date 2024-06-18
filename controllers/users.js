0eed032a6b9dc7444e421f27c7f44f6056ea31c4
Aimar Kiivits <Aimarkiivits1@gmail.com>
2024-06-18 22:01:19 +0300

    Fix existing user and password limitation errors

--- controllers/users.js
+++ controllers/users.js
@@ -3,23 +3,47 @@ const User = require('../models/user');
 
 const register = (req, res) => {
     console.log(req.body);
-    bcrypt.hash(req.body.password, 10, (err, cryptPassword) => {
-        User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: cryptPassword
-        })
-        .then(registered => {
-            req.session.user = {
-                username: registered.username,
-                user_id: registered.id
-            };
-            console.log(req.session)
-            res.json({
-                message: 'User registered successfully',
-                user: registered,
-                session: req.session.user
+
+    if (!req.body.username || !req.body.email || !req.body.password) {
+        return res.status(400).json({
+            message: 'All fields are required'
+        });
+    }
+    if (req.body.password.length < 5) {
+        return res.status(400).json({
+            message: 'Password must be at least 5 characters long'
+        });
+    }
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(user => {
+        if (user) {
+            return res.status(400).json({
+                message: 'User with this username already exists'
             });
+        }
+
+        bcrypt.hash(req.body.password, 10, (err, cryptPassword) => {
+            User.create({
+                username: req.body.username,
+                email: req.body.email,
+                password: cryptPassword
+            })
+            .then(registered => {
+                req.session.user = {
+                    username: registered.username,
+                    user_id: registered.id
+                };
+                console.log(req.session)
+                res.json({
+                    message: 'User registered successfully',
+                    user: registered,
+                    session: req.session.user
+                });
+            })
         })
     })
 }
